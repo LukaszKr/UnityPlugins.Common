b1939c2441d96fc5922759332cfcb88036a92412
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2021-01-03 21:25:26 +0100

    Thread Helper

---
+++ Runtime/MultiThreading/ThreadHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProceduralLevel.UnityPlugins.Common.MultiThreading
+{
+	public static class ThreadHelper
+	{
+		#region Thread
+		private static readonly Task[] m_TaskBuffer;
+		#endregion
+
+		static ThreadHelper()
+		{
+			m_TaskBuffer = new Task[Math.Max(1, SystemInfo.processorCount)];
+		}
+
+		public static void RunAndWaitAll(Action action)
+		{
+			int taskCount = m_TaskBuffer.Length;
+
+			for(int x = 0; x < taskCount; ++x)
+			{
+				m_TaskBuffer[x] = Task.Run(action);
+			}
+			Task.WaitAll(m_TaskBuffer);
+			for(int x = 0; x < taskCount; ++x)
+			{
+				m_TaskBuffer[x].Dispose();
+				m_TaskBuffer[x] = null;
+			}
+		}
+	}
+}
