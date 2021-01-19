8803ee56464a53f44d4fe72002d4da544242882d
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2021-01-19 18:45:21 +0100

    gizmos ext

--- Runtime/Ext/GUIExt.cs
+++ Runtime/Ext/GUIExt.cs
@@ -6,6 +6,8 @@ namespace ProceduralLevel.UnityPlugins.Common
 	public static class GUIExt
 	{
 		private static readonly Stack<Color> m_ColorStack = new Stack<Color>();
+		private static readonly Stack<Color> m_BackgroundColorStack = new Stack<Color>();
+		private static readonly Stack<Color> m_ContentColorStack = new Stack<Color>();
 
 		public static void PushColor(Color color)
 		{
@@ -16,6 +18,28 @@ namespace ProceduralLevel.UnityPlugins.Common
 		public static void PopColor()
 		{
 			GUI.color = m_ColorStack.Pop();
+		}
+
+		public static void PushBackgroundColor(Color color)
+		{
+			m_BackgroundColorStack.Push(GUI.color);
+			GUI.backgroundColor = color;
+		}
+
+		public static void PopBackgroundColor()
+		{
+			GUI.color = m_BackgroundColorStack.Pop();
+		}
+
+		public static void PushContentColor(Color color)
+		{
+			m_ContentColorStack.Push(GUI.color);
+			GUI.contentColor = color;
+		}
+
+		public static void PopContentColor()
+		{
+			GUI.color = m_ContentColorStack.Pop();
 		}
 	}
 }
