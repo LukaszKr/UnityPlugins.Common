1af58a3f4bebdf29c32097de3cd18777c98e7e75
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2022-11-19 23:40:32 +0100

    ID classes

---
+++ Logic/ID/UGuid/UGuidJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ProceduralLevel.UnityPlugins.Common.Logic
+{
+	internal class UGuidJsonConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			string strValue = value.ToString();
+			writer.WriteValue(strValue);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			string rawValue = (string)reader.Value;
+			return Activator.CreateInstance(objectType, rawValue);
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(UGuid<>) == objectType;
+		}
+	}
+}
---
+++ Logic/ID/UID/UIDJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ProceduralLevel.UnityPlugins.Common.Logic
+{
+	public class UIDJsonConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value.GetHashCode());
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			int intValue = int.Parse(reader.Value.ToString());
+			return Activator.CreateInstance(objectType, intValue);
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return true;
+		}
+	}
+}
