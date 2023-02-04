f85d999924229398ab162ee5aa164959571bfbad
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-02-04 19:22:15 +0100

    revert transform override tool

---
+++ Editor/Tools/RevertTransformOverride.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ProceduralLevel.UnityPlugins.Common.Editor
+{
+	internal static class RevertTransformOverride
+	{
+		private const string NAME = "Revert Overrides";
+
+		private const string REVERT_TRANSFORM = "CONTEXT/"+nameof(Transform)+"/"+NAME;
+		private const string REVERT_RECT_TRANSFORM = "CONTEXT/"+nameof(RectTransform)+"/"+NAME;
+
+		[MenuItem(REVERT_TRANSFORM)]
+		[MenuItem(REVERT_RECT_TRANSFORM)]
+		private static void Revert(MenuCommand command)
+		{
+			var serObj = new SerializedObject(command.context);
+			var prop = serObj.GetIterator();
+			while(prop.NextVisible(true))
+			{
+				PrefabUtility.RevertPropertyOverride(prop, InteractionMode.UserAction);
+			}
+		}
+
+		[MenuItem(REVERT_TRANSFORM, validate = true)]
+		[MenuItem(REVERT_RECT_TRANSFORM, validate = true)]
+		private static bool Validate(MenuCommand command)
+		{
+			var obj = command.context;
+			return PrefabUtility.IsPartOfPrefabInstance(obj);
+		}
+	}
+}
