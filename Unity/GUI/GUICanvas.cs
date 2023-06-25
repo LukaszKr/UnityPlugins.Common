d5a3995ed7da833adbe10588e97dcd7d3c110972
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-06-25 20:30:30 +0200

    GUI Canvas for debug editors

---
+++ Unity/GUI/GUICanvas.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ProceduralLevel.UnityPlugins.Common.Unity
+{
+	public class GUICanvas
+	{
+		public readonly int ReferenceWidth;
+		public readonly int ReferenceHeight;
+
+		public float Scale;
+		public float ScaleX;
+		public float ScaleY;
+
+		public int Width;
+		public int Height;
+
+		public GUICanvas(int width, int height)
+		{
+			ReferenceWidth = width;
+			ReferenceHeight = height;
+		}
+
+		public void Use()
+		{
+			Width = Screen.width;
+			Height = Screen.height;
+			ScaleX = Width/(float)ReferenceWidth;
+			ScaleY = Height/(float)ReferenceHeight;
+			Scale = Mathf.Max(ScaleX, ScaleY);
+			Width = Mathf.CeilToInt(Width/Scale);
+			Height = Mathf.CeilToInt(Height/Scale);
+			GUI.matrix = Matrix4x4.Scale(new Vector3(Scale, Scale, Scale));
+		}
+	}
+}
