d12abd1d6563fbebe4d8929e75508cf1ada84e3c
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-07-14 18:13:27 +0200

    namespace based json type converter

---
+++ Logic/Serialization/NamespaceTypeJsonConverter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ProceduralLevel.Common.Logic
+{
+	public class NamespaceTypeConverter : JsonConverter
+	{
+		public const string TYPE_FIELD = "$field";
+
+		public readonly string Namespace;
+		public readonly string Assembly;
+		private bool _canWrite = true;
+
+		public override bool CanWrite => _canWrite;
+
+		public NamespaceTypeConverter(string namespaceAndAssembly)
+		{
+			Namespace = namespaceAndAssembly;
+			Assembly = namespaceAndAssembly;
+		}
+		public NamespaceTypeConverter(string ns, string asm)
+		{
+			Namespace = ns;
+			Assembly = asm;
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			_canWrite = false;
+			JObject obj = JObject.FromObject(value);
+			obj[TYPE_FIELD] = value.GetType().Name;
+			obj.WriteTo(writer);
+			_canWrite = true;
+		}
+
+		private Type GetType(JObject jObject)
+		{
+			string typeName = (string)jObject.Property(TYPE_FIELD);
+			string fullTypeName = $"{Namespace}.{typeName}, {Assembly}";
+			Type simpleType = Type.GetType(fullTypeName);
+			return simpleType;
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			// return typeof(T).IsAssignableFrom(objectType);
+			return true;
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType,
+			object existingValue, JsonSerializer serializer)
+		{
+			if(reader.TokenType == JsonToken.Null)
+			{
+				return null;
+			}
+
+			JObject jObject = JObject.Load(reader);
+			Type targetType = GetType(jObject);
+			if(targetType == null)
+			{
+				return null;
+			}
+
+			object target = Activator.CreateInstance(targetType, true);
+			serializer.Populate(jObject.CreateReader(), target);
+			return target;
+		}
+	}
+}
