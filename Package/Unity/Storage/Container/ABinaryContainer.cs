f40500456ccbaa0b12621dcf986d8e1dfcbc4911
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2024-01-28 23:11:02 +0100

    json container

---
+++ Package/Unity/Storage/Container/ABinaryContainer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using ProceduralLevel.Common.Ext;
+using UnityEngine;
+
+namespace ProceduralLevel.Common.Unity
+{
+	public abstract class ABinaryContainer<TValue> : ADataContainer<TValue>
+		where TValue : class, IBinarySerializable
+	{
+		[SerializeField, HideInInspector]
+		private byte[] m_BinaryData;
+
+		public override void Load()
+		{
+			if(m_BinaryData == null)
+			{
+				m_Value = Create();
+				return;
+			}
+
+			try
+			{
+				m_Value = Create(m_BinaryData.ToBinaryReader());
+			}
+			catch(Exception e)
+			{
+				Debug.LogException(e, this);
+				m_Value = Create();
+			}
+		}
+
+		protected override void Save()
+		{
+			using(MemoryStream stream = new MemoryStream())
+			{
+				using(BinaryWriter writer = new BinaryWriter(stream))
+				{
+					m_Value.WriteToBuffer(writer);
+					m_BinaryData = stream.ToArray();
+				}
+			}
+		}
+
+		protected abstract TValue Create();
+		protected abstract TValue Create(BinaryReader reader);
+	}
+}
---
+++ Package/Unity/Storage/Container/ADataContainer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace ProceduralLevel.Common.Unity
+{
+	public abstract class ADataContainer<TValue> : ScriptableObject, ISerializationCallbackReceiver
+		where TValue : class
+	{
+		protected TValue m_Value;
+
+		public TValue Value
+		{
+			get { return m_Value; }
+			set { m_Value = value; }
+		}
+
+		public abstract void Load();
+		protected abstract void Save();
+
+#if UNITY_EDITOR
+		public void Editor_Save()
+		{
+			Save();
+			UnityEditor.EditorUtility.SetDirty(this);
+		}
+
+		public void Editor_RecordObject(string reason)
+		{
+			UnityEditor.Undo.RecordObject(this, reason);
+		}
+#endif
+
+		#region Serialization Callbacks
+		public void OnBeforeSerialize()
+		{
+			Save();
+		}
+
+		public void OnAfterDeserialize()
+		{
+			Load();
+		}
+		#endregion
+	}
+}
