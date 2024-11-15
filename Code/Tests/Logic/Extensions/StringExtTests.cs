03b666ac3576047bd1f17363579760d41b2227da
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2024-11-15 18:07:52 +0100

    string extension test

---
+++ Code/Tests/Logic/Extensions/StringExtTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace UnityPlugins.Common.Logic.Ext
+{
+	[Category(CommonTestsConsts.CATEGORY_ASSEMBLY)]
+	internal class StringExtTests
+	{
+		[Test]
+		public void JoinToString()
+		{
+			List<string> list = new List<string>()
+			{
+				"Hello",
+				"World"
+			};
+			Assert.AreEqual("Hello, World", list.JoinToString());
+		}
+	}
+}
