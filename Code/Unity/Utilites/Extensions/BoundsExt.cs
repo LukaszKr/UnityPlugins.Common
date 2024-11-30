56208fc8677bf2aad1a19a3f43f41ebcf90174c6
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2024-11-30 23:15:28 +0100

    bounds extension

---
+++ Code/Unity/Utilites/Extensions/BoundsExt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityPlugins.Common.Logic;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class BoundsExt
+	{
+		public static void GetCorners(this Bounds bounds, Vector3[] corners)
+		{
+			GameAssert.AreEqual(8, corners.Length);
+			Vector3 min = bounds.min;
+			Vector3 max = bounds.max;
+			Vector3 size = bounds.size;
+			corners[0] = min;
+			corners[1] = min;
+			corners[1].x += size.x;
+			corners[2] = min;
+			corners[2].y += size.y;
+			corners[3] = min;
+			corners[3].z += size.z;
+
+			corners[4] = max;
+			corners[5] = max;
+			corners[5].x -= size.x;
+			corners[6] = max;
+			corners[6].y -= size.y;
+			corners[7] = max;
+			corners[7].z -= size.z;
+		}
+	}
+}
