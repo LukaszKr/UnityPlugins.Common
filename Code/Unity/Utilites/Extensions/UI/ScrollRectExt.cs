49d7237f4cd10daa3a6ee1a6bc3432958a9f0e1d
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-01-16 22:20:54 +0100

    scroll rect extension

---
+++ Code/Unity/Utilites/Extensions/UI/ScrollRectExt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class ScrollRectExt
+	{
+		public static Vector2 GetSnapToPositionToBringChildIntoView(this ScrollRect scrollRect, RectTransform child)
+		{
+			Vector2 contentPos = (Vector2)scrollRect.transform.InverseTransformPoint(scrollRect.content.position);
+			Vector2 childPos = (Vector2)scrollRect.transform.InverseTransformPoint(child.position);
+			Vector2 endPos = contentPos - childPos;
+			//keep selected in middle if possible
+			RectTransform rectTransform = scrollRect.GetComponent<RectTransform>();
+			endPos.x += rectTransform.rect.width*rectTransform.pivot.x;
+			endPos.y -= rectTransform.rect.height*rectTransform.pivot.y;
+
+			if(!scrollRect.horizontal)
+			{
+				endPos.x = contentPos.x;
+			}
+			if(!scrollRect.vertical)
+			{
+				endPos.y = contentPos.y;
+			}
+			return endPos;
+		}
+	}
+}
