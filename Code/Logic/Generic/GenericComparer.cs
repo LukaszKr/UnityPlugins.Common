c486c53b46f15505bf3c3ef9e44c790dd61eb7cd
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-01-19 13:22:13 +0100

    generic comparer

---
+++ Code/Logic/Generic/GenericComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityPlugins.Common.Logic
+{
+	public class GenericComparer<T> : IComparer<T>
+		where T : IComparable<T>
+	{
+		public static GenericComparer<T> Instance = new GenericComparer<T>();
+
+		private GenericComparer()
+		{
+		}
+
+		public int Compare(T x, T y)
+		{
+			return x.CompareTo(y);
+		}
+	}
+}
