d2b528c37973f2c74c5b33a74e8939f7216f6f12
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-03-01 10:42:42 +0100

    simple scene reference class

---
+++ Code/Editor/Unity/Scenes/SceneRefPropertyDrawer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityPlugins.Common.Unity;
+
+namespace UnityPlugins.Common.Editor
+{
+	[CustomPropertyDrawer(typeof(SceneRef))]
+	public class SceneRefPropertyDrawer : AExtendedPropertyDrawer
+	{
+		protected override void Draw(Rect position, SerializedProperty property, GUIContent label)
+		{
+			Draw(position, label, 
+				property.FindPropertyRelative(nameof(SceneRef.Name)),
+				property.FindPropertyRelative(nameof(SceneRef.Guid))
+			);
+		}
+
+		private void Draw(Rect position, GUIContent label, SerializedProperty sceneName, SerializedProperty assetGuid)
+		{
+			SceneAsset scene = null;
+			string guid = assetGuid.stringValue;
+			if(!string.IsNullOrEmpty(guid))
+			{
+				string path = AssetDatabase.GUIDToAssetPath(assetGuid.stringValue);
+				if(!string.IsNullOrEmpty(path))
+				{
+					scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(path);
+				}
+			}
+
+			SceneAsset newScene = (SceneAsset)EditorGUILayout.ObjectField(label, scene, typeof(SceneAsset), false);
+			if(newScene != null)
+			{
+				sceneName.stringValue = newScene.name;
+				string assetPath = AssetDatabase.GetAssetPath(newScene);
+				assetGuid.stringValue = AssetDatabase.GUIDFromAssetPath(assetPath).ToString();
+			}
+			else
+			{
+				sceneName.stringValue = string.Empty;
+				assetGuid.stringValue = string.Empty;
+			}
+		}
+	}
+}
---
+++ Code/Unity/Utilites/Scenes/SceneRef.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityPlugins.Common.Unity
+{
+	[Serializable]
+	public class SceneRef
+	{
+		public string Guid;
+		public string Name;
+
+		public void Load()
+		{
+			Scene scene = SceneManager.GetActiveScene();
+			SceneManager.LoadScene(Name);
+		}
+
+		public void Load(LoadSceneMode sceneMode)
+		{
+			SceneManager.LoadScene(Name, sceneMode);
+		}
+
+		public AsyncOperation LoadAsync()
+		{
+			return SceneManager.LoadSceneAsync(Name);
+		}
+
+		public AsyncOperation LoadAsync(LoadSceneMode sceneMode)
+		{
+			return SceneManager.LoadSceneAsync(Name, sceneMode);
+		}
+
+		public AsyncOperation UnloadAsync()
+		{
+			return SceneManager.UnloadSceneAsync(Name);
+		}
+	}
+}
