465f30d6be68dbd5dd1084130f44bd6061d0fc94
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-03-03 10:19:13 +0100

    set layer extension

---
+++ Code/Unity/Utilites/Extensions/GameObjectExt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class GameObjectExt
+	{
+		public static void SetLayerToHierarchy(this Transform transform, int layer)
+		{
+			transform.gameObject.layer = layer;
+			int childCount = transform.childCount;
+			for(int x = 0; x < childCount; ++x)
+			{
+				Transform child = transform.GetChild(x);
+				child.SetLayerToHierarchy(layer);
+			}
+		}
+	}
+}
