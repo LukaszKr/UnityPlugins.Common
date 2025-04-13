d08e4b5760f66e486269ca9043faca295cde7df2
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-04-13 22:46:45 +0200

    revert validators in storage entry

--- Code/Unity/Utilites/Storage/Utilities/StorageEntry.cs
+++ Code/Unity/Utilites/Storage/Utilities/StorageEntry.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using UnityPlugins.Common.Logic;
 
 namespace UnityPlugins.Common.Unity
 {
 	public class StorageEntry<TValue>
 	{
-		[JsonIgnore]
-		public readonly List<IValueConstraint<TValue>> Validators = new List<IValueConstraint<TValue>>();
-
 		[JsonProperty]
 		private TValue m_Value;
 		[JsonProperty]
@@ -51,8 +47,6 @@ namespace UnityPlugins.Common.Unity
 
 		public bool Set(TValue value)
 		{
-			value = EvaluateConstraints(value);
-
 			if(!Equals(m_Value, value))
 			{
 				m_Modified = true;
@@ -63,23 +57,11 @@ namespace UnityPlugins.Common.Unity
 			return false;
 		}
 
-		private TValue EvaluateConstraints(TValue value)
-		{
-			int count = Validators.Count;
-			for(int x = 0; x < count; ++x)
-			{
-				IValueConstraint<TValue> validator = Validators[x];
-				value = validator.Evaluate(value);
-			}
-			return value;
-		}
-
 		[OnDeserialized]
 		internal void OnAfterDeserialize(StreamingContext context)
 		{
 			if(m_Modified)
 			{
-				m_Value = EvaluateConstraints(m_Value);
 				return;
 			}
 			m_Value = m_DefaultValue;
