15c3880992f2172eab2d808feeaa77cc898663c0
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-05-03 09:23:33 +0200

    extended coroutine

---
+++ Code/Unity/Utilites/Extensions/ExtendedCoroutine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityPlugins.Common.Unity
+{
+	public class ExtendedCoroutine
+	{
+		private MonoBehaviour m_Parent;
+		private Coroutine m_Current;
+
+		public bool IsActive => (m_Current != null);
+
+		~ExtendedCoroutine()
+		{
+			Kill();
+		}
+
+		public void Start(MonoBehaviour parent, IEnumerator coroutine)
+		{
+			Kill();
+			m_Parent = parent;
+			m_Current = parent.StartCoroutine(CoroutineWrapper(coroutine));
+		}
+
+		public void Kill()
+		{
+			if(m_Current != null)
+			{
+				if(m_Parent)
+				{
+					m_Parent.StopCoroutine(m_Current);
+				}
+				m_Current = null;
+			}
+		}
+
+		private IEnumerator CoroutineWrapper(IEnumerator coroutine)
+		{
+			yield return coroutine;
+			m_Current = null;
+		}
+	}
+}
