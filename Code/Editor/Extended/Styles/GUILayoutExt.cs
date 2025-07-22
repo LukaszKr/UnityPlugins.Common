f352666ec80d223962f105f4693613aa066f4277
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-07-22 12:20:28 +0200

    GUILayoutExt - easier optional button

---
+++ Code/Editor/Extended/Styles/GUILayoutExt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityPlugins.Common.Editor
+{
+	public static class GUILayoutExt
+	{
+		public static bool Button(string label, bool disabled)
+		{
+			bool clicked = false;
+
+			EditorGUI.BeginDisabledGroup(disabled);
+			{
+				if(GUILayout.Button(label))
+				{
+					clicked = true;
+				}
+			}
+			EditorGUI.EndDisabledGroup();
+
+			return clicked;
+		}
+	}
+}
