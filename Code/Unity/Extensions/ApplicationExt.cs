53826f3defdb919d07cbd256542ab6b2b97d04e9
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-08-25 22:53:05 +0200

    Quit method that works in editor

---
+++ Code/Unity/Extensions/ApplicationExt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class ApplicationExt
+	{
+		/// <summary>
+		/// Quit that works in editor
+		/// </summary>
+		public static void Quit()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#endif
+			Application.Quit();
+		}
+	}
+}
