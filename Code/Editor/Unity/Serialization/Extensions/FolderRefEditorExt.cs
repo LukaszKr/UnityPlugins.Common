2f52064ad0358aa15d200529043734f0cd390d0a
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-08-26 12:03:51 +0200

    folder reference editor extension

---
+++ Code/Editor/Unity/Serialization/Extensions/FolderRefEditorExt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityPlugins.Common.Unity;
+
+namespace UnityPlugins.Common.Editor
+{
+	public static class FolderRefEditorExt
+	{
+		public static string GetPath(this FolderRef folder)
+		{
+			return AssetDatabase.GetAssetPath(folder.Value);
+		}
+	}
+}
