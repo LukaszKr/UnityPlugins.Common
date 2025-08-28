1d85d2f7272c4859f8e2b3c44a47f37c2bf13971
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-08-28 10:34:53 +0200

    Get editor GUID of object/asset method

---
+++ Code/Unity/Extensions/ObjectExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using Object = UnityEngine.Object;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class ObjectExt
+	{
+#if UNITY_EDITOR
+		public static string GetEditorGUID(this Object target)
+		{
+			if(target == null)
+			{
+				throw new NullReferenceException();
+			}
+			string path = AssetDatabase.GetAssetPath(target);
+			return AssetDatabase.AssetPathToGUID(path);
+		}
+#endif
+	}
+}
