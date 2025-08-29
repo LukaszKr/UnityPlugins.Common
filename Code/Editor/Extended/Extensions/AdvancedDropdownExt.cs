1fd1d4b4609c4d343092f1e31a2edf98fd94a774
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-08-29 22:05:14 +0200

    advanced dropdown extension method

---
+++ Code/Editor/Extended/Extensions/AdvancedDropdownExt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace UnityPlugins.Common.Editor
+{
+	public static class AdvancedDropdownExt
+	{
+		public static void ShowAtCurrentMousePosition(this AdvancedDropdown dropdown)
+		{
+			Rect rect = GUILayoutUtility.GetLastRect();
+			rect.width = Screen.width;
+			rect.position = Event.current.mousePosition;
+			dropdown.Show(rect);
+		}
+	}
+}
