eb35edb268c52aaf217bf97f2b78ad061b1de31c
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-09-10 00:23:01 +0200

    shuffle list extension

---
+++ Code/Logic/Extensions/ListExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityPlugins.Common.Logic
+{
+	public static class ListExt
+	{
+		public static void Shuffle<T>(this IList<T> list, Random random)
+		{
+			int n = list.Count;
+			while(n > 1)
+			{
+				n--;
+				int k = random.Next(n + 1);
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
+		}
+	}
+}
