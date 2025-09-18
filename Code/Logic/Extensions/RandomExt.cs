6dded3910c4d33e5f7fb858818616efd53648bf5
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-09-18 17:59:02 +0200

    generate seed method

---
+++ Code/Logic/Extensions/RandomExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityPlugins.Common.Logic
+{
+	public static class RandomExt
+	{
+		public static int GenerateSeed()
+		{
+			return Guid.NewGuid().GetHashCode();
+		}
+	}
+}
