f0f8f2d0fbe5a8b022ed1a21fe02b26b9ddcdd21
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-09-19 09:41:55 +0200

    fix issue with jsonconverters getting stripped away from the builds

---
+++ Code/Unity/Serialization/JsonStrippingPreventer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine.Scripting;
+
+namespace UnityPlugins.Common.Logic
+{
+	/// <summary>
+	/// Purpose of this is to prevent Unity from removing json converters without using link.xml which won't work according to documentation, in packages.
+	/// I can't  apply Preserve tags to these converts, are they are in no-unity assembly.
+	/// </summary>
+	internal static class JsonStrippingPreventer
+	{
+		[Preserve]
+		public static JsonSerializerSettings GetSettings()
+		{
+			return new JsonSerializerSettings()
+			{
+				Converters = new List<JsonConverter>()
+				{
+					new UGuidJsonConverter(),
+					new IDJsonConverter(),
+					new GridSize2DJsonConverter(),
+					new GridSize3DJsonConverter(),
+					new GridPoint2DJsonConverter(),
+					new GridPoint3DJsonConverter(),
+				}
+			};
+		}
+	}
+}
