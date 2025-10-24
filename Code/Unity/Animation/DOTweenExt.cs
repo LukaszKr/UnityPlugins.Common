e37800565bd3cc0f058cc47368034af5c1018245
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-10-24 20:41:52 +0200

    DoTween Ext

---
+++ Code/Unity/Animation/DOTweenExt.cs
@@ -0,0 +1,13 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class DOTweenExt
+	{
+		public static Tween DOSize(this SpriteRenderer renderer, Vector2 target, float duration)
+		{
+			return DOTween.To(() => renderer.size, (v) => { renderer.size = v; }, target, duration);
+		}
+	}
+}
