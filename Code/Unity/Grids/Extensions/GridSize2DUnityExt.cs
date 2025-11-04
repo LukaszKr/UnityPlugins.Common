1c110a566739132d0800e452dd7a1950c61051fd
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-11-04 18:59:38 +0100

    grid size extension

---
+++ Code/Unity/Grids/Extensions/GridSize2DUnityExt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityPlugins.Common.Logic;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class GridSize2DUnityExt
+	{
+		public static Vector2 ToVector2(this GridSize2D size)
+		{
+			Vector2 vec = default;
+			vec.x = size.X;
+			vec.y = size.Y;
+			return vec;
+		}
+
+		public static Vector2Int ToVector3Int(this GridSize2D size)
+		{
+			Vector2Int vec = default;
+			vec.x = size.X;
+			vec.y = size.Y;
+			return vec;
+		}
+	}
+}
