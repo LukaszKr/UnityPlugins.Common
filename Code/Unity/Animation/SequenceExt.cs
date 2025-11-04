b3f2145985194aab1f5aa8f6f275d121a414e2ab
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-11-04 20:14:27 +0100

    DOTween sequence extension

---
+++ Code/Unity/Animation/SequenceExt.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using DG.Tweening;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class SequenceExt
+	{
+		public static Sequence TryAppend(this Sequence sequence, Tween tween)
+		{
+			if(tween != null)
+			{
+				sequence.Append(tween);
+			}
+			return sequence;
+		}
+
+		public static Sequence TryJoin(this Sequence sequence, Tween tween)
+		{
+			if(tween != null)
+			{
+				sequence.Join(tween);
+			}
+			return sequence;
+		}
+	}
+}
