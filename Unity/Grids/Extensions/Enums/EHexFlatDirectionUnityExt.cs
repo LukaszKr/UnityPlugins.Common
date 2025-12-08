a7101e67fca19673ef0e14b488931412971e69f2
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-12-08 15:45:05 +0100

    HexPointyDirection extension

---
+++ Unity/Grids/Extensions/Enums/EHexFlatDirectionUnityExt.cs
@@ -0,0 +1,22 @@
+﻿using UnityPlugins.Common.Logic;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class EHexFlatDirectionUnityExt
+	{
+		private static readonly float[] m_Rotations = new float[] //assuming up is the default
+		{
+			0f,
+			60f,
+			120f,
+			180f,
+			240f,
+			300f,
+		};
+
+		public static float ToRotation(this EHexFlatDirection direction)
+		{
+			return m_Rotations[(int)direction];
+		}
+	}
+}
---
+++ Unity/Grids/Extensions/Enums/EHexPointyDirectionUnityExt.cs
@@ -0,0 +1,22 @@
+﻿using UnityPlugins.Common.Logic;
+
+namespace UnityPlugins.Common.Unity
+{
+	public static class EHexPointyDirectionUnityExt
+	{
+		private static readonly float[] m_Rotations = new float[] //assuming right is the default - easier to work with
+		{
+			-60f,
+			0f,
+			60f,
+			120f,
+			180f,
+			240f,
+		};
+
+		public static float ToRotation(this EHexPointyDirection direction)
+		{
+			return m_Rotations[(int)direction];
+		}
+	}
+}
